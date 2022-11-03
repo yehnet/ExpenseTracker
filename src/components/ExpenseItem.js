eb92ae5509b3e39baf1c908dea0092be924b4e7c
yehnet <yehnet@post.bgu.ac.il>
2022-11-03 20:20:01 +0200

    data props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 10, 3);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 2449.2;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">₪{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">₪{props.amount}</div>
       </div>
     </div>
   );
