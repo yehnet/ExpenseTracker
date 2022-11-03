fe7d7d0d9335f49c21fe75e20918812939413786
yehnet <yehnet@post.bgu.ac.il>
2022-11-03 19:58:44 +0200

    Start Project

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>3rd November 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>₪2449.20</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
