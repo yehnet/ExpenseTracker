375aecdab468acc821edbcb5785b42d4b4b6a063
yehnet <yehnet@post.bgu.ac.il>
2022-11-16 21:13:05 +0200

    Two-Way Binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -29,6 +29,9 @@ function ExpenseForm() {
     };
 
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -36,7 +39,11 @@ function ExpenseForm() {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -44,6 +51,7 @@ function ExpenseForm() {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -53,6 +61,7 @@ function ExpenseForm() {
             type="date"
             min="2022-01-01"
             step="2023-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
